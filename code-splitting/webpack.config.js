a29968b60420502b8d7646f18dde3f57e2a75b27
chaelin_kim <chaelin_kim@tmax.co.kr>
2022-05-03 18:30:06 +0900

    feat: plugin 사용

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "none",
@@ -15,13 +16,16 @@ module.exports = {
       {
         test: /\.css$/,
         // 모든 css 파일에 대해
-        use: ["style-loader", "css-loader"],
+        // use: ["style-loader", "css-loader"],
         // 적용할 모듈
-        // 로더는 오른쪽에서 왼쪽으로 적용 된다.
         // ex) sass-loader 를 사용할 때
         // use: ["style-loader", "css-loader", "sass-loader"],
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
       },
     ],
   },
   // module = loader
+  // 비 자바스크립트 파일을 웹팩이 인식할 수 있게 추가하는 속성. 로더는 오른쪽에서 왼쪽 순으로 적용
+  plugins: [new MiniCssExtractPlugin()],
+  // 웹팩으로 변환한 파일에 추가적인 기능을 더하고 싶을 때 사용하는 속성. 웹팩 변환 과정 전반에 대한 제어권을 갖고 있음
 };
