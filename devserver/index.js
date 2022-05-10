da86f42587c95b43edd511a64b7450618c39fa82
chaelin_kim <chaelin_kim@tmax.co.kr>
2022-05-10 17:00:34 +0900

    feat: css 로더 추가

--- devserver/index.js
+++ devserver/index.js
@@ -1,2 +1,4 @@
+import "./base.css";
+
 var div = document.querySelector(".container");
 div.innerText = "웹팩 데브서버 돌렸다!!";
--- devserver/webpack.config.js
+++ devserver/webpack.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "none",
@@ -7,6 +8,14 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
+      },
+    ],
   },
   devServer: {
     port: 9010,
@@ -16,5 +25,6 @@ module.exports = {
       // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
       template: "index.html",
     }),
+    new MiniCssExtractPlugin({ filename: "main.css" }),
   ],
 };
